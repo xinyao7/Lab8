9b569be81b1817a1382678c356d16e6923106aa8
Xinyao <xinyao7@ualberta.ca>
2023-03-10 14:58:29 -0700

    Update CustomList.java

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -49,6 +49,10 @@ public class CustomList extends ArrayAdapter<City> {
         return cities.size();
     }
 
+    /**
+     * this adds a city object to the list
+     * @param city
+     */
     public void addCity(City city){
         list = MockCityList();
         int listSize = list.getCount();
